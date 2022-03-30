f4fbb08f53427eac952486ecc77753e71f1b0d8a
Wiktor Musial <wiktormusial@icloud.com>
2022-03-30 21:44:43 +0200

    Add offset Pagination

--- src/components/Launches/index.tsx
+++ src/components/Launches/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@apollo/client/react";
+import { useQuery } from "@apollo/client";
 import { GET_LAUNCHES } from "../../graphql/queries/GET_LAUNCHES";
 import { LaunchList, LaunchVars } from "../../types/launches";
 
--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,8 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_API_URL,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache({
+        typePolicies: {
+            Query: {
+                fields: {
+                    launches: offsetLimitPagination()
+                }
+            }
+        }
+    })
 })
 
 export default client
