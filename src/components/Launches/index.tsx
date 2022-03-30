e64d44f7b80a58761034930b4984106111b863d9
Wiktor Musial <wiktormusial@icloud.com>
2022-03-30 22:00:44 +0200

    Fetch more data

--- src/components/Launches/index.tsx
+++ src/components/Launches/index.tsx
@@ -1,30 +1,55 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, NetworkStatus } from "@apollo/client";
+import { useState } from "react";
 import { GET_LAUNCHES } from "../../graphql/queries/GET_LAUNCHES";
 import { LaunchList, LaunchVars } from "../../types/launches";
 import LaunchesListElement from "./LaunchesListElement";
 
 const Launches = () => {
-  const { loading, error, data } = useQuery<LaunchList, LaunchVars>(
-    GET_LAUNCHES,
-    {
-      variables: {
-        limit: 10,
-        offset: 0,
-      },
-    }
-  );
+  const [limit, setLimit] = useState(10);
 
-  if (loading) return <p>Loading...</p>;
+  const { error, data, networkStatus, fetchMore } = useQuery<
+    LaunchList,
+    LaunchVars
+  >(GET_LAUNCHES, {
+    variables: {
+      limit: limit,
+      offset: 0,
+    },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (networkStatus === NetworkStatus.loading && !data) {
+    return <div>Loading...</div>;
+  }
+
   if (error) return <div>{error}</div>;
 
   return (
     <div>
       Launches list
+      <br />
+      <button
+        onClick={() => {
+          const length = data!.launches.length;
+
+          fetchMore({
+            variables: {
+              limit: 10,
+              offset: length,
+            },
+          }).then((res) => {
+            setLimit(length + res.data.launches.length);
+          });
+        }}
+      >
+        Fetch more
+      </button>
       <ul>
         {data &&
           data.launches.map((item) => {
             return <LaunchesListElement key={item.id} item={item} />;
           })}
+        {networkStatus === NetworkStatus.fetchMore && <li>Loading</li>}
       </ul>
     </div>
   );
